224ed5657573e652b09f6ef3dfa88bd40fa609d5
mohab <mohab.batta@gmail.com>
2022-08-15 00:27:26 +0200

    first commit

---
+++ PodCast/SongItem.swift
@@ -0,0 +1,23 @@
+//
+//  SongItem.swift
+//  PodCast
+//
+//  Created by mohab on 8/14/22.
+//
+
+import UIKit
+
+class SongItem: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PodCast/SongModel.swift
@@ -0,0 +1,8 @@
+//
+//  SongModel.swift
+//  PodCast
+//
+//  Created by mohab on 8/14/22.
+//
+
+import Foundation
